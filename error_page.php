4e9546b09ad6131c7f1e5ab2a711377c5b677113
ka1t0_k1d <100038173+M1nh-Duk@users.noreply.github.com>
2023-06-05 20:38:35 +0700

    Update error_page.php

--- error_page.php
+++ error_page.php
@@ -5,15 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>ERROR</title>
 </head>
 <body>
     <h1>Something went wrong! Please try again!</h1>
     <h2><a href="bit.ly/3MPL8Sm">Details</a></h2>
 </body>
 </html>
-<?php
-
-    echo "Congratulations !"
-
-?>
